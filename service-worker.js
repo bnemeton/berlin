ce606af74d4fb7332d0bb00fb156cf3d91d0284a
Joe <jmwinitsky@gmail.com>
2021-05-20 21:03:27 -0700

    changing service worker

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,54 @@
-// TODO: add listener and handler to cache static assets
-const filesToCache = [
-    '/',
-    './manifest.webmanifest',
-    './index.html',
-    './assets/css/style.css',
-    './assets/images/brandenburg.jpg',
-    './assets/images/logo16.png',
-    './assets/images/logo48.png',
-    './assets/images/logo128.png',
-    './assets/images/logo512.png',
-    './assets/images/map.jpg',
-    './assets/images/reichstag.jpg'
-]
+// // TODO: add listener and handler to cache static assets
+// const filesToCache = [
+//     '/',
+//     './manifest.webmanifest',
+//     './index.html',
+//     './assets/css/style.css',
+//     './assets/images/brandenburg.jpg',
+//     './assets/images/logo16.png',
+//     './assets/images/logo48.png',
+//     './assets/images/logo128.png',
+//     './assets/images/logo512.png',
+//     './assets/images/map.jpg',
+//     './assets/images/reichstag.jpg'
+// ]
 
 
-// TODO: add listener and handler to retrieve static assets from the Cache Storage in the browser 
-self.addEventListener('install', (event) => {
-    event.waitUntil(caches.open('static')
-    .then(cache => {return cache.addAll(filesToCache)}));
+// // TODO: add listener and handler to retrieve static assets from the Cache Storage in the browser 
+// self.addEventListener('install', (event) => {
+//     event.waitUntil(caches.open('static')
+//     .then(cache => {return cache.addAll(filesToCache)}));
+//     self.skipWaiting();
+// })
+// self.addEventListener('fetch', (event) => {
+//     event.respondWith(caches.match(event.request)
+//     .then(response => {
+//         return response || fetch(event.request)
+//     }));
+// })
+
+// install event handler
+self.addEventListener('install', event => {
+    event.waitUntil(
+      caches.open('static').then( cache => {
+        return cache.addAll([
+          './',
+          './index.html',
+          './assets/css/style.css',
+          './assets/images/brandenburg.jpg',
+          './assets/images/reichstag.jpg',
+          './assets/images/map.jpg',
+        ]);
+      })
+    );
+    console.log('Install');
     self.skipWaiting();
-})
-self.addEventListener('fetch', (event) => {
-    event.respondWith(caches.match(event.request)
-    .then(response => {
-        return response || fetch(event.request)
-    }));
-})
+  });
+  // retrieve assets from cache
+  self.addEventListener('fetch', event => {
+    event.respondWith(
+      caches.match(event.request).then( response => {
+        return response || fetch(event.request);
+      })
+    );
+  });
